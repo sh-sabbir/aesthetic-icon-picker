a95c0b74f842a4f078d62e4a9b4f9fe06065aeae
Sabbir <sabbirshouvo@gmail.com>
2021-01-20 15:14:46 +0600

    improved markup

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,74 +6,88 @@
 		// console.log(brands);
 		// console.log('hello');
 		var iconLibrary = {
-			'brands' : brands,
-			'regular' : regular,
-			'solid' : solid,
+			'brands': brands,
+			'regular': regular,
+			'solid': solid,
 			// 'happy-icons' : happyIcons, // load font & css before on it
 		};
 
 		var iconMarkup = '';
 
-		$.each( iconLibrary, function( iconPackKey, iconPack ) {
+		$.each(iconLibrary, function (iconPackKey, iconPack) {
 
 			// console.log( iconPack['icons'] );
 
 			var prefix = brandKey(iconPackKey);
 
-			$.each( iconPack['icons'], function( key, icon ) {
+			$.each(iconPack['icons'], function (key, icon) {
 
 				var cls = prefix + icon;
 
-				iconMarkup += iconItem(iconPackKey, cls, icon );
+				iconMarkup += iconItem(iconPackKey, cls, icon);
 
 				// console.log( icon );
 
-			 });
+			});
 
 		});
 
-		
+
 
 		// TODO: Push ALL icon in window load
-		$('#elementor-icons-manager__tab__content').append( iconMarkup );
+		$('#aesthetic-icons-manager__tab__content').append(iconMarkup);
 
-		function iconItem(brand,iconCls,filter){
-			var $markup = '<div class="elementor-icons-manager__tab__item" data-library="'+brand+'" filter="'+filter+'"><div class="elementor-icons-manager__tab__item__content"><i class="'+iconCls+'"></i><div class="elementor-icons-manager__tab__item__name" title="'+ filter +'">'+ filter +'</div></div></div>';
+		function iconItem(brand, iconCls, filter) {
+			var $markup = '<div class="aesthetic-icons-manager__tab__item" data-library="' + brand + '" filter="' + filter + '"><div class="aesthetic-icons-manager__tab__item__content"><i class="' + iconCls + '"></i><div class="aesthetic-icons-manager__tab__item__name" title="' + filter + '">' + filter + '</div></div></div>';
 			return $markup;
 		}
 
-		function brandKey(key){
-			switch(key) {
+		function brandKey(key) {
+			switch (key) {
 				case 'brands':
-				  return 'fab fa-';
-				  break;
+					return 'fab fa-';
+					break;
 				case 'solid':
-				  return 'fas fa-';
-				  break;
+					return 'fas fa-';
+					break;
 				case 'regular':
-				  return 'far fa-';
-				  break;
+					return 'far fa-';
+					break;
 				case 'happy-icons':
-				  return 'hm hm-';
-				  break;
+					return 'hm hm-';
+					break;
 				default:
 					return 'fab fa-';
-			  }
+			}
 		}
 		// TODO: sidebar class
-		$('.elementor-icons-manager__tab-link').on('click', function () {
+		$('.aesthetic-icons-manager__tab-link').on('click', function () {
+			/* Handle Icon Pack Active State */
+			$(this).parent().find('.aesthetic-active').removeClass("aesthetic-active");
+			$(this).addClass("aesthetic-active");
 
-			console.log($(this).data('library-id'));
+			/* Filter Icons by Pack */
+			var library = $(this).data('library-id');
+			var icons = $('#aesthetic-icons-manager__tab__content').children();
 
+			if (library != "all") {
+				icons.filter(function () {
+					$(this).toggle($(this).data('library') == library);
+				});
+			} else {
+				icons.filter(function () {
+					$(this).show();
+				});
+			}
 		});
 
 		// TODO: search input
-		$('#elementor-icons-manager__search input').on("keyup", function() {
+		$('#aesthetic-icons-manager__search input').on("keyup", function () {
 			var value = $(this).val().toLowerCase();
-			var icon = $('#elementor-icons-manager__tab__content').find('i');
+			var icon = $('#aesthetic-icons-manager__tab__content').find('i');
 			//console.log( value );
-			icon.filter(function() {
-				$(this).closest('.elementor-icons-manager__tab__item').toggle($(this).attr('class').toLowerCase().indexOf(value) > -1);
+			icon.filter(function () {
+				$(this).closest('.aesthetic-icons-manager__tab__item').toggle($(this).attr('class').toLowerCase().indexOf(value) > -1);
 			});
 		});
 
